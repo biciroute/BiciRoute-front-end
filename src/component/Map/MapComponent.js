73101959cba15d6887d1390878250348c8c2f6e4
CrkJohn <Jhonhrge@gmail.com>
2019-09-07 19:33:46 -0500

    already paints

--- src/component/Map/MapComponent.js
+++ src/component/Map/MapComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleApiWrapper, Map, Marker, } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Marker, Polyline } from 'google-maps-react';
 import "./Map.css"
 import Button from '@material-ui/core/Button';
 import NavigationIcon from '@material-ui/icons/Navigation';
@@ -7,8 +7,6 @@ import Modal from '@material-ui/core/Modal';
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
-import Script from 'react-load-script';
-import SearchBar from './SearchBar';
 
 
 
@@ -45,10 +43,16 @@ class MapComponent extends React.Component {
         this.state = {
             university: { lat: 4.782715, lng: -74.042611 },
             open: false,
+            pathRoute: [],
         };
+
+        // Modal
         this.handleClose = this.handleClose.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
+        // Route
         this.autocomplete = this.autocomplete.bind(this);
+        this.setDirectionRoute = this.setDirectionRoute.bind(this);
+        //this.renderDirections =  this.renderDirections.bind(this);
 
     }
 
@@ -84,26 +88,67 @@ class MapComponent extends React.Component {
         })
     }
 
+    async setDirectionRoute(){
+        var newPathRoute = await this.calculateRoute();
+
+        console.log(newPathRoute);
+        this.setState({pathRoute: newPathRoute.overview_path});
+
+     
+    }
+
 
     autocomplete() {
         const { google, map } = this.props;
         if (!google || !map) return;
-        window.alert("funciona")
         var src = document.getElementById("source");
-        window.alert(src);
-        //var tgt = document.getElementById('target');
+        var tgt = document.getElementById('target');
         var autoSrc = new google.maps.places.Autocomplete(src);
+        var autoTgt = new google.maps.places.Autocomplete(tgt);
         autoSrc.bindTo('bounds', map);
         autoSrc.setFields(['address_components', 'geometry', 'icon', 'name']);
-
+        autoTgt.bindTo('bounds', map);
+        autoTgt.setFields(['address_components', 'geometry', 'icon', 'name']);
     }
+
+
+    calculateRoute(travelMode, origin, destination, dateUniversity) {
+        const { google, map } = this.props;
+        if (!google || !map) return;
+        const directionsService = new google.maps.DirectionsService();
+        let pathRoute;
+        window.alert("Entro "  + document.getElementById("source").value +  "  "  );
+        const request = {
+            origin: document.getElementById("source").value,
+            destination: document.getElementById("target").value,
+            travelMode: "DRIVING",
+        };
+        return new Promise((resolve, reject) => {
+            directionsService.route(request, (response, status) => {
+                if (status === 'OK'){
+                    window.alert("OK PASS");
+                    pathRoute = response.routes[0];
+                    resolve(pathRoute)
+                } else {
+
+                    window.alert('Directions request failed due to ' + status);
+                    reject(status)
+                }
+            })
+        }
+        )
+    }
+
+    
+
+
 
     //https://stackoverflow.com/questions/26059762/callback-when-dom-is-loaded-in-react-js
     componentDidMount() {
         this.autocomplete();
-        /*if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.setCurrentPosition)
-        }*/
+        if (navigator && navigator.geolocation) {
+            //navigator.geolocation.getCurrentPosition(this.setCurrentPosition)
+        }
     }
 
     componentDidUpdate(prevProps) {
@@ -124,8 +169,7 @@ class MapComponent extends React.Component {
                     google={this.props.google}
                     zoom={15}
                     style={mapStyles}
-                    initialCenter={this.state.university}
-                >
+                    initialCenter={this.state.university}>
                     <Marker
                         title={'Escuela colombiana de ingenieria Julio Garavito'}
                         position={this.state.university}
@@ -133,26 +177,66 @@ class MapComponent extends React.Component {
                         name={'Escuela colombiana de ingenieria Julio Garavito'}
                         description={'AK 45 #205-59 Bogota\nInstitucion universitaria'}
                     />
+
+                    <Polyline
+                        path={this.state.pathRoute}
+                        geodesic={false}
+                        options={{
+                            strokeColor: '#38B44F',
+                            strokeOpacity: 1,
+                            strokeWeight: 7,
+                        }}
+                    />
                     <Button title="Begin your route with biciRoute" variant="contained" color="primary" onClick={this.handleOpen} id="buttonSearch" aria-label="delete" >
                         <NavigationIcon /> Search trip
                     </Button>
+
+
                 </Map>
 
+                <TextField
+                    id="source"
+                    type="search"
+                    label="trip's start"
+                    className={classes.textField}
+                    margin="normal"
+                />
+
+                <TextField
+                    id="target"
+                    type="search"
+                    label="trip's end"
+                    className={classes.textField}
+                    margin="normal"
+                />
+
+                <Button variant="contained" color="primary" onClick={this.setDirectionRoute}>
+                    Search possible trips
+                </Button>
 
 
-                <Modal
-                    aria-labelledby="simple-modal-title"
-                    aria-describedby="simple-modal-description"
+                <Modal onLoad={this.autocomplete}
                     open={this.state.open}
                     onClose={this.handleClose}
                 >
                     <div className={classes.paper}>
                         <TextField
-                            id='source'
+                            id="source"
                             type="search"
                             className={classes.textField}
                             margin="normal"
                         />
+
+                        <TextField
+                            id="target"
+                            type="search"
+                            className={classes.textField}
+                            margin="normal"
+                        />
+
+
+
+
                         <h2 id="simple-modal-title">Text in a modal</h2>
                         <p id="simple-modal-description">
                             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
@@ -178,6 +262,6 @@ const MapWrapper = props => (
 );
 
 export default withStyles(useStyles)(GoogleApiWrapper({
-    apiKey: '',
+    apiKey: 'AIzaSyAC9GvNPhAIg4NDnqeCBItE0SzmYLLUBtY',
     language: "es",
 })(MapWrapper));
