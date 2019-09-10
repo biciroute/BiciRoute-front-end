f89f43e6565d426fe29dce60cd261fb32fd734e3
AndresFlorez <santiago.rocha@tulpep.com>
2019-09-10 18:03:02 -0500

    finish SideBar without colors

--- src/UserTab.js
+++ src/UserTab.js
@@ -6,13 +6,13 @@ export class UserTab extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="tab-container">
                 <div className="logo-content">
                     <img src={logo}></img>
                 </div>
                 <div className="name-content">
-                    <span className="text">Hola,</span>
-                    <span className="text-name">Santiago</span>
+                    <p>Hola,</p>
+                    <h1>Santiago</h1>
                 </div>
             </div>
         );
