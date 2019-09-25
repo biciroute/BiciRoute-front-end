471897faaa8f3bf9ab39ef12adf6ce747b4af3a3
CrkJohn <Jhonhrge@gmail.com>
2019-09-24 23:33:00 -0500

    Fixing problem localStorage

--- src/component/Map/MapComponent.js
+++ src/component/Map/MapComponent.js
@@ -185,6 +185,27 @@ export class MapComponent extends React.Component {
             pathRouteOriginPlace: theBestOriginToPlace[2],
             open: false
         });
+
+        var newJSON = JSON.stringify( {
+            origin : origin,
+            destination :  destination,
+            pathRouteDestinationPlace : theBestDestinationToPlace[1],
+            pathRouteOriginPlace : theBestOriginToPlace[1]
+        });
+
+        if (localStorage.getItem('lastroutes') === undefined || localStorage.getItem('lastroutes') === null ) {
+            localStorage.setItem('lastroutes', JSON.stringify([newJSON]))
+        }else{
+            var tdListJSON = []
+            var tdLists = JSON.parse(localStorage.getItem("lastroutes"));
+            for(var i = 0 ; i < tdLists.length  ; ++i ){
+                tdListJSON.push(tdLists[i]);
+            } 
+            tdListJSON.push(newJSON);
+            localStorage.setItem("lastroutes",JSON.stringify(tdListJSON));
+        }
+
+
         const places = [origin, destination, theBestDestinationToPlace[1], theBestOriginToPlace[1]]
         this.setState({
             markers: []
